46b8161cbf21734de2be41667d9c4f167c714bd2
Muri3M <mmmurilo@hotmail.com>
2022-10-24 17:43:46 -0300

    fix: mui import and initial content

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,22 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import CssBaseline from '@mui/material/CssBaseline'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>cowCulate</title>
+        <meta
+          name="cowCulate"
+          content="App para ajudar a vizualizar sem problema"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
