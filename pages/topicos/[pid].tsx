9d1bf8c34dd6cbffc2a90df3e16adc88bee542d0
Pedro <pedro.005henr@gmail.com>
2022-11-13 19:36:30 -0300

    routing fix

--- pages/topicos/[pid].tsx
+++ pages/topicos/[pid].tsx
@@ -28,7 +28,7 @@ const getThumbnailImages = async (page: string, subtopico: string) => {
   await db.collection('thumbnail_images').doc(page).collection(subtopico).get()
   .then((snapshot) => {
     snapshot.docs.forEach((document) => {
-      thumbsImages.push({title: document.data().id, image: document.data().url})
+      thumbsImages.push({title: document.id, image: document.data().url})
     })
   })
   
