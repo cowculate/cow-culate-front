08303655236217a0851a3ffda7e17ec903aaa123
Pedro <pedro.005henr@gmail.com>
2022-12-02 15:54:23 -0300

    style: changed imagebutton component style and title style

--- components/atoms/ImageButton/index.tsx
+++ components/atoms/ImageButton/index.tsx
@@ -14,7 +14,9 @@ const ImageButtonComponent: React.FC<ImageButtonInterface> = ({
   onClick,
 }) => {
   return (
-    <ImageButton
+    <ImageButton sx = {{
+      boxShadow: "rgba(255, 255, 255, 0.4) -5px 5px, rgba(255, 255, 255, 0.3) -10px 10px, rgba(255, 255, 255, 0.2) -15px 15px, rgba(255, 255, 255, 0.1) -20px 20px, rgba(255, 255, 255, 0.05) -25px 25px;"
+    }}
       focusRipple
       key={image.title}
       style={{ backgroundImage: `url(${image.url})` }}
--- components/molecules/TopicContent/index.tsx
+++ components/molecules/TopicContent/index.tsx
@@ -38,6 +38,7 @@ const TopicContent: React.FC<TopicContentInterface> = ({ topics }) => {
               />
             ))}
           </ButtonsWrapper>
+          <br/><br/>
         </>
       ))}
     </Wrapper>
