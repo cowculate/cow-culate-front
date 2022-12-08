e5985cd6fe3248a0bd11b3c1708d5e242ae02f58
Pedro <pedro.005henr@gmail.com>
2022-12-08 20:14:41 -0300

    feat/restyled-header-and-footer

--- components/molecules/Footer/style.ts
+++ components/molecules/Footer/style.ts
@@ -2,6 +2,7 @@ import { styled } from '@mui/material'
 
 export const Wrapper = styled('div') ({
     display: 'flex',
+    borderTop: '1px solid white',
     backgroundColor: 'black',
     margin: '0',
     height: '',
