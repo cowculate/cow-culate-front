fb228c16d9d39ba8285edd263d9280f320bd52d6
Pedro <pedro.005henr@gmail.com>
2022-12-08 18:46:26 -0300

    feat/created-footer-component

---
+++ components/molecules/Footer/style.ts
@@ -0,0 +1,12 @@
+import { styled } from '@mui/material'
+
+export const Wrapper = styled('div') ({
+    display: 'flex',
+    backgroundColor: 'black',
+    margin: '0',
+    height: '',
+})
+
+export const ProductName = styled('div') ({ 
+    fontSize: '10px',
+})
